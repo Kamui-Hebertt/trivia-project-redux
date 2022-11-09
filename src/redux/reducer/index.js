504d4394b5bfa7128e31967458e442593a055d51
Diogo Martins <me.martinsfe@gmail.com>
2022-11-09 17:07:48 -0300

    feat: configura o Redux

---
+++ src/redux/reducer/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import player from './player';
+import trivia from './trivia';
+
+const rootReducer = combineReducers({
+  player,
+  trivia,
+});
+
+export default rootReducer;
