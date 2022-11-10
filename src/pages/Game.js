6db4182fa0e4bef2b354f3848e17816b5ebe6b8e
Diogo Martins <me.martinsfe@gmail.com>
2022-11-10 18:10:07 -0300

    feat: adiciona logica para verificar o token e redirecionar para o login caso o token seja invalido

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,20 +7,45 @@ class Game extends Component {
   constructor(props) {
     super(props);
 
-    const { dispatch } = this.props;
+    this.state = {
+      questions: {},
+      erro: '',
+      responseCode: 0,
+      isFetching: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { dispatch, history } = this.props;
     const tokenFromStorage = localStorage.getItem('token');
-    dispatch(requestTrivia(tokenFromStorage));
+    await dispatch(requestTrivia(tokenFromStorage));
+
+    const { erro, responseCode } = this.props;
+    this.setState({ erro, responseCode, isFetching: false });
+
+    if (responseCode === 3) {
+      localStorage.removeItem('token');
+      history.push('/');
+    }
   }
 
   render() {
+    const { isFetching } = this.state;
     return (
 
       <div>
         <h1>Tela do jogo</h1>
         <Header />
+        {isFetching ? <p>Loading</p> : 'Perguntas'}
       </div>
     );
   }
 }
 
-export default connect()(Game);
+const mapStateToProps = (state) => ({
+  questions: state.trivia.questions,
+  erro: state.trivia.erro,
+  responseCode: state.trivia.responseCode,
+});
+
+export default connect(mapStateToProps)(Game);
