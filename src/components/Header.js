caf12949643e5031273033fe3e91d70231b06c22
Kamui-Hebertt <heberttaferreira.com@gmail.com>
2022-11-10 12:40:24 -0300

    estado global sendo renderizado no header, com a imagem gravatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import md5 from 'crypto-js/md5';
 import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
-    const { name, score } = this.props;
+    const { name, score, email } = this.props;
+    const hash1 = md5(email).toString();
     return (
       <div>
-        <img data-testid="header-profile-picture" src="" alt="perfil" />
+        <img
+          data-testid="header-profile-picture"
+          src={ `https://www.gravatar.com/avatar/${hash1}` }
+          alt={ name }
+
+        />
         <p data-testid="header-player-name">{name}</p>
         <p data-testid="header-score">
           Pontuação :
@@ -21,7 +28,7 @@ class Header extends Component {
 
 const mapStateToProps = (state) => ({
   name: state.player.name,
-  // email: stte.player.userEmail,
+  email: state.player.gravatarEmail,
   score: state.player.score,
 });
 
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Settings extends Component {
   render() {
     return (
-      <div>Tela de configurações</div>
+      <div><h1>Tela de configurações</h1></div>
     );
   }
 }
