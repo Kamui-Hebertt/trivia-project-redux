c8b3c12442e40bdc747da7be09d6051a714157c0
Diogo Martins <me.martinsfe@gmail.com>
2022-11-12 18:11:20 -0300

    feat: cria função para exibir mensagem de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
+
+const TRES = 3;
 
 class Feedback extends Component {
+  renderScoreFeedback = () => {
+    const { assertions } = this.props;
+    if (assertions >= TRES) {
+      return 'Well Done!';
+    }
+
+    return 'Could be better...';
+  };
+
   render() {
     return (
       <div>
-        <h1 data-testid="feedback-text">Pagina de Feedback</h1>
+        <h1>Pagina de Feedback</h1>
+        <Header />
+        <p data-testid="feedback-text">{this.renderScoreFeedback()}</p>
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (store) => ({
+  assertions: store.player.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
