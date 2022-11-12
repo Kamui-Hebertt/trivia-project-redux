2b9976e5d843a521100128d327b7a5649b5f8e8f
Diogo Martins <me.martinsfe@gmail.com>
2022-11-12 18:40:23 -0300

    feat: adiciona rota para página de ranking

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Feedback from './pages/Feedback';
 import Game from './pages/Game';
 import Login from './pages/Login';
+import Ranking from './pages/Ranking';
 import Settings from './pages/Settings';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <Route exact path="/playgame" component={ Game } />
       <Route exact path="/settings" component={ Settings } />
       <Route exact path="/feedback" component={ Feedback } />
+      <Route exact path="/ranking" component={ Ranking } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
