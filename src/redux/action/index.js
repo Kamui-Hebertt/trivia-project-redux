10209f3c8f242d353622200b6347f35aafda71d0
Diogo Martins <me.martinsfe@gmail.com>
2022-11-12 19:48:19 -0300

    feat: cria ação para modificar o nivel de dificuldade do jogo

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,6 +1,7 @@
 export const USER = 'USER';
 export const UPDATE_SCORE = 'UPDATE_SCORE';
 export const START_NEW_GAME = 'START_NEW_GAME';
+export const GAME_DIFFICULTY = 'GAME_DIFFICULTY';
 
 export const userInfo = (state) => ({
   type: USER,
@@ -14,4 +15,9 @@ export const updateScore = (payload) => ({
 
 export const newGame = () => ({
   type: START_NEW_GAME,
+});
+
+export const setGameDifficulty = (payload) => ({
+  type: GAME_DIFFICULTY,
+  payload,
 });
