9829d0b5f0743dce065ca1e38aa4244478d9dacf
Diogo Martins <me.martinsfe@gmail.com>
2022-11-12 06:36:36 -0300

    feat: cria funcao para calcular pontos
    Cria tambem a renderizacao condicional do botao Next, e junto com o botao,,
    que redireciona para a pagina de feedback apos ter respondido a todas as
    questoes
    
    Refatora codigo para melhor interacao entre as funcoes

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,9 +2,22 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
-// import { requestTrivia } from '../redux/action/fetch';
+import { updateScore } from '../redux/action';
 
-// const TRES = 3;
+const DIFFICULTY_BONUS_SCORE = {
+  hard: 3,
+  medium: 2,
+  easy: 1,
+};
+
+const UM_SEGUNDO = 1000;
+const CINCO_SEGUNDOS = 5000;
+const TRINTA_UM_SEGUNDOS = 31000;
+const PONTUCAO_ACERTO = 10;
+
+let startTime = null;
+let countdown5seg = null;
+let countdown30seg = null;
 
 class Game extends Component {
   constructor(props) {
@@ -12,30 +25,17 @@ class Game extends Component {
 
     this.state = {
       questions: {},
-      // erro: '',
-      // responseCode: 0,
-      // isFetching: true,
+      isFetching: true,
       questionNumber: null,
+      countdownTimer: 30,
+      shuffledAnswers: [],
+      isDisabled: false,
     };
   }
 
   async componentDidMount() {
     const { history } = this.props;
     const tokenFromStorage = localStorage.getItem('token');
-    // await dispatch(requestTrivia(tokenFromStorage));
-
-    // const { erro, responseCode, questions } = this.props;
-    // this.setState(() => ({
-    //   erro,
-    //   responseCode,
-    //   isFetching: false,
-    //   questionNumber: 0,
-    //   questions }));
-
-    // if (responseCode === TRES || erro) {
-    //   localStorage.removeItem('token');
-    //   history.push('/');
-    // }
 
     const URL = `https://opentdb.com/api.php?amount=5&token=${tokenFromStorage}`;
     const response = await fetch(URL);
@@ -47,45 +47,120 @@ class Game extends Component {
     }
     this.setState({
       questions: data.results,
-      // isFetching: false,
+      isFetching: false,
       questionNumber: 0,
-    });
+    }, this.shuffleAnswers);
   }
+
+  stopTimer = () => {
+    clearInterval(startTime);
+    clearTimeout(countdown5seg);
+    clearTimeout(countdown30seg);
+  };
+
+  gettingReady = () => {
+    this.stopTimer();
+    countdown5seg = setTimeout(() => {
+      this.countDown();
+    }, CINCO_SEGUNDOS);
+  };
+
+  disableAnswers = () => {
+    this.setState({ isDisabled: true });
+  };
+
+  timer = () => {
+    this.setState((prev) => ({
+      countdownTimer: prev.countdownTimer - 1,
+    }));
+  };
+
+  countDown = () => {
+    const { isDisabled } = this.state;
+
+    if (!isDisabled) {
+      startTime = setInterval(this.timer, UM_SEGUNDO);
+
+      countdown30seg = setTimeout(() => {
+        this.stopTimer();
+        this.disableAnswers();
+      }, TRINTA_UM_SEGUNDOS);
+    }
+  };
+
+  shuffleAnswers = () => {
+    const { questions, questionNumber, shuffledAnswers } = this.state;
+
+    if (!shuffledAnswers[0]) {
+      const shufflingAnswers = [];
+      const answers = [questions[questionNumber]?.correct_answer,
+        questions[questionNumber]?.incorrect_answers].flat();
+
+      do {
+        shufflingAnswers
+          .push(...answers.splice([Math.floor(Math.random() * answers.length)], 1));
+      } while (answers.length > 0);
+
+      this.setState({ shuffledAnswers: shufflingAnswers }, () => this.gettingReady());
+    }
+  };
+
+  calcScore = (answer) => {
+    const { countdownTimer, questions, questionNumber } = this.state;
+    const { dispatch } = this.props;
+    const { correct_answer: correctAnswer, difficulty } = questions[questionNumber];
+
+    // console.log('Tempo restante:', countdownTimer);
+    // console.log('Resposta correta:', correctAnswer);
+    // console.log('Bonus por dificuldade:', DIFFICULTY_BONUS_SCORE[difficulty]);
+
+    if (answer === correctAnswer) {
+      const pointsScored = PONTUCAO_ACERTO
+      + (countdownTimer * DIFFICULTY_BONUS_SCORE[difficulty]);
+
+      dispatch(updateScore(pointsScored));
+    }
+  };
 
   handleAnswer = ({ target }) => {
     const { questions, questionNumber } = this.state;
-    // console.log(target.parentElement);
     const parent1 = target.parentElement.childNodes;
     parent1.forEach((item) => {
-      // item.textContent === questions[questionNumber].correct_answer
-      //   ? item.style.border = '3px solid rgb(6, 240, 15)'
-      //   : item.style.border = '3px solid red';
       if (item.textContent === questions[questionNumber].correct_answer) {
         item.style.border = '3px solid rgb(6, 240, 15)';
       } else {
         item.style.border = '3px solid red';
       }
     });
+
+    this.stopTimer();
+    this.disableAnswers();
+    this.calcScore(target.textContent);
+  };
+
+  handleNextQuestion = () => {
+    const { questionNumber, questions } = this.state;
+    const { history } = this.props;
+
+    if (questionNumber === questions.length - 1) {
+      history.push('/feedback');
+    }
+
+    this.setState((prev) => ({
+      questionNumber: prev.questionNumber + 1,
+      shuffledAnswers: [],
+      countdownTimer: 30,
+      isDisabled: false,
+    }), () => this.shuffleAnswers());
   };
 
   render() {
     const {
       questions,
-      // question,
-      // erro,
-      // responseCode,
-      // isFetching,
-      questionNumber } = this.state;
-
-    const answers = [questions[questionNumber]?.correct_answer,
-      questions[questionNumber]?.incorrect_answers].flat().sort();
-
-    const shuffledAnswers = [];
-
-    do {
-      shuffledAnswers
-        .push(...answers.splice([Math.floor(Math.random() * answers.length)], 1));
-    } while (answers.length > 0);
+      isDisabled,
+      isFetching,
+      questionNumber,
+      shuffledAnswers } = this.state;
 
     return (
 
@@ -93,7 +168,8 @@ class Game extends Component {
         <h1>Tela do jogo</h1>
         <Header />
         {
-          !shuffledAnswers[0] ? <p>Loading</p>
+          isFetching
+            ? <p>Loading</p>
             : (
               <div>
                 <h2
@@ -111,10 +187,11 @@ class Game extends Component {
                     shuffledAnswers.map((it, index) => (
                       <button
                         onClick={ this.handleAnswer }
+                        disabled={ isDisabled }
                         key={ index }
                         type="button"
-                        data-testid={ it === questions[questionNumber]
-                          ?.correct_answer ? 'correct-answer'
+                        data-testid={ it === questions[questionNumber]?.correct_answer
+                          ? 'correct-answer'
                           : `wrong-answer-${index}` }
                       >
                         {it}
@@ -122,6 +199,17 @@ class Game extends Component {
                     ))
                   }
                 </div>
+
+                { isDisabled
+                && (
+                  <button
+                    type="button"
+                    data-testid="btn-next"
+                    onClick={ this.handleNextQuestion }
+                  >
+                    Next
+                  </button>
+                )}
 
               </div>
             )
