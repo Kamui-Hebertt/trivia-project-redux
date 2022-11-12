6cc1e2d6fc353810a4197384dc6755bd9615992a
Diogo Martins <me.martinsfe@gmail.com>
2022-11-12 18:13:51 -0300

    feat: cria uma action para zerar a pontuação ao iniciar novo jogo

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,5 +1,6 @@
 export const USER = 'USER';
 export const UPDATE_SCORE = 'UPDATE_SCORE';
+export const START_NEW_GAME = 'START_NEW_GAME';
 
 export const userInfo = (state) => ({
   type: USER,
@@ -9,4 +10,8 @@ export const userInfo = (state) => ({
 export const updateScore = (payload) => ({
   type: UPDATE_SCORE,
   payload,
+});
+
+export const newGame = () => ({
+  type: START_NEW_GAME,
 });
