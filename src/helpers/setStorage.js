d274ea515e38b551724e13d48fbab41e60396386
Diogo Martins <me.martinsfe@gmail.com>
2022-11-17 08:19:25 -0300

    refact: adiciona regra de negócio
    Ao popular o localStorage será verificado se o mesmo "existe", e então o valor anterior será "espalhado" concatenando o valor do novo jogador

--- src/helpers/setStorage.js
+++ src/helpers/setStorage.js
@@ -1,5 +1,17 @@
-const setLocal = (player) => {
-  localStorage.setItem('players', JSON.stringify(player));
+import getLocal from './getStorage';
+
+const setLocal = (newPlayer) => {
+  // Quando o jogador na página ranking, será criado um localStorage
+  // caso não exista ao menos uma posição no array (localStorage !== true)
+  if (!JSON.parse(localStorage.getItem('playersRank'))) {
+    localStorage.setItem('playersRank', JSON.stringify([]));
+  }
+
+  const playersAlreadyInLocal = getLocal();
+  localStorage.setItem('playersRank', JSON.stringify(
+    [...playersAlreadyInLocal,
+      newPlayer],
+  ));
 };
 
 export default setLocal;
