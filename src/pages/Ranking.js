4f0ec92082509bef40d86a395b95e1cd7218749f
Diogo Martins <me.martinsfe@gmail.com>
2022-11-17 11:07:12 -0300

    ffix: modifica como o sort() é feito
    Troquei o isinal que compara (>) por (-)
    Obrigado Serjão! Sergio Francisco, Summer de Instrução

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import getLocal from '../helpers/getStorage';
 
 class Ranking extends Component {
+  state = {
+    rank: [],
+  };
+
+  componentDidMount() {
+    const playersToRank = getLocal();
+    this.setState({ rank: [...playersToRank] });
+  }
+
   pressToBeBackBtn = () => {
     const { history } = this.props;
     history.push('/');
@@ -14,11 +25,24 @@ class Ranking extends Component {
     // const hash1 = md5(gravatarEmail).toString();
     // const hash2 = md5(localStorage.getItem('player')[2]);
     // console.log(localStorage.getItem('score'));
+    const { rank } = this.state;
+    const sortAlreadySortedRank = rank.sort((a, b) => b.score - a.score);
     return (
       <div>
         <h1 data-testid="ranking-title">
           Página de Ranking
         </h1>
+
+        <div>
+          {sortAlreadySortedRank?.map((player, index) => (
+            <div key={ index }>
+              <img src={ `https://www.gravatar.com/avatar/${md5(player.gravatarEmail).toString()}` } alt={ `Jogador ${player.name}` } />
+
+              <p data-testid={ `player-name-${index}` }>{player.name}</p>
+              <p data-testid={ `player-score-${index}` }>{player.score}</p>
+            </div>
+          ))}
+        </div>
 
         <button
           type="button"
